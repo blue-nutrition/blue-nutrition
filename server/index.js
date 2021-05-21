49ce75f1ab9c0ba812d104f939de7b8506e595d5
Ben Lambert <kalstarn@gmail.com>
2021-05-21 17:18:10 -0600

    db skeleton added

--- server/index.js
+++ server/index.js
@@ -10,6 +10,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('./client/dist'));
 
+db.on('aquire', (connection) => {
+  console.log('Connection aquired: ', connection.threadId);
+})
+
+db.on('connection', () => {
+  connection.query('SET SESSION auto_increment_increment=1')
+});
+
+db.on('connection', function (connection) {
+  connection.query('SET SESSION auto_increment_increment=1')
+});
+
+db.on('enqueue', function () {
+  console.log('Waiting for available connection slot');
+});
+
+db.on('release', function (connection) {
+  console.log('Connection %d released', connection.threadId);
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to mysql database.');
+});
+
+
 app.listen(port, () => {
   console.log('the freaking thing worked dammit');
 })
