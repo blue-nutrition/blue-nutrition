4d2bf37c72f9a97798557d22e026842573974c9e
a-clowes <clowes.alison@gmail.com>
2021-05-25 11:11:53 -0600

    removed commented out code

--- client/src/Context.jsx
+++ client/src/Context.jsx
@@ -38,35 +38,3 @@ export const ContextProvider = (props) => {
 }
 
 
-// import React, { createContext, useState } from 'react';
-
-// export const AppContext = createContext();
-
-// export const ContextProvider = (props) => {
-
-//   // list of useState items
-//   const [exampleState, setExampleState] = useState('Hello World');
-//   const [currentWeight, setCurrentWeight] = useState(0);
-//   const [waterGoal, setWaterGoal] = useState(64);
-//   const [caloriesGoal, setCaloriesGoal] = useState(2000);
-//   const [proteinMacrosGoal, setProteinMacrosGoal] = useState(50);
-//   const [carbsMacrosGoal, setCarbsMacrosGoal] = useState(100);
-//   const [fatsMacrosGoal, setFatsMacrosGoal] = useState(25);
-//   const [goalWeight, setGoalWeight] = useState();
-
-
-//   return (
-//     <AppContext.Provider value={{
-//       exampleState, setExampleState,
-//       currentWeight, setCurrentWeight,
-//       waterGoal, setWaterGoal,
-//       caloriesGoal, setCaloriesGoal,
-//       proteinMacrosGoal, setProteinMacrosGoal,
-//       carbsMacrosGoal, setCarbsMacrosGoal,
-//       fatsMacrosGoal, setFatsMacrosGoal,
-//       goalWeight, setGoalWeight
-//     }}>
-//       {props.children}
-//     </AppContext.Provider>
-//   )
-// }
