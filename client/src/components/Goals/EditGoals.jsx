7e2eeb54ce9eb4dfdb18d56b135f355da0f1d9f3
a-clowes <clowes.alison@gmail.com>
2021-05-24 20:08:34 -0600

    updated edit goals

--- client/src/components/Goals/EditGoals.jsx
+++ client/src/components/Goals/EditGoals.jsx
@@ -5,39 +5,45 @@ import { AppContext } from '../../Context.jsx';
 
 const EditGoals = ({ handleClose }) => {
   const {
-    currentWeight, setCurrentWeight, waterGoal, setWaterGoal, caloriesGoal, setCaloriesGoal, proteinMacrosGoal, setProteinMacrosGoal, carbsMacrosGoal, setCarbsMacrosGoal, fatsMacrosGoal, setFatsMacrosGoal, goalWeight, setGoalWeight
+    email, password, userId, userGoals, setUserGoals
   } = useContext(AppContext);
 
-  const [editedWeight, setEditedWeight] = useState(currentWeight);
-  const [editedWaterGoal, setEditedWaterGoal] = useState(waterGoal);
-  const [editedCaloriesGoal, setEditedCaloriesGoal] = useState(caloriesGoal);
-  const [editedProteinMacrosGoal, setEditedProteinMacrosGoal] = useState(proteinMacrosGoal);
-  const [editedCarbsMacrosGoal, setEditedCarbsMacrosGoal] = useState(carbsMacrosGoal);
-  const [editedFatsMacrosGoal, setEditedFatsMacrosGoal] = useState(fatsMacrosGoal);
-  const[editedGoalWeight, setEditedGoalWeight] = useState(goalWeight);
+  const [editedWeight, setEditedWeight] = useState(userGoals.weight)
+  const [editedGoals, setEditedGoals] = useState({
+    water: userGoals.water,
+    calories: userGoals.calories,
+    protein: userGoals.protein,
+    carbs: userGoals.carbs,
+    fats: userGoals.fats,
+    goalWeight: userGoals.goalWeight
+  })
 
-  const postData = {
-    user,
-    currentWeight: editedWeight,
-    waterGoal: editedWaterGoal,
-    caloriesGoal: editedCaloriesGoal,
-    proteinMacrosGoal: editedProteinMacrosGoal,
-    carbsMacrosGoal: editedCarbsMacrosGoal,
-    fatsMacrosGoal: editedFatsMacrosGoal,
-    goalWeight: editedGoalWeight
+  const weightData = {
+    userId,
+    weight: editedWeight,
+    date,
   }
 
-  const setGoals = (postData) => {
-    axios.post('/goals', postData)
+  const userData = {
+    email,
+    password,
+    goals: editedGoals
+  }
+
+  const setUser = (userData, weightData) => {
+    axios.post('/user', userData)
       .then(results => {
-        setCurrentWeight(editedWeight);
-        setWaterGoal(editedWaterGoal);
-        setCaloriesGoal(editedCaloriesGoal);
-        setProteinMacrosGoal(editedProteinMacrosGoal);
-        setCarbsMacrosGoal(editedCarbsMacrosGoal);
-        setFatsMacrosGoal(editedFatsMacrosGoal);
-        setGoalWeight(editedGoalWeight);
-        handleClose();
+        axios.post('/weight', weightData)
+        .then(results => {
+          setWeight(editedWeight);
+          setWaterGoal(editedWater);
+          setCaloriesGoal(editedCalories);
+          setProteinGoal(carbsGoal);
+          setCarbsGoal(editedCarbs);
+          setFatsGoal(editedFats);
+          setGoalWeight(editedGoalWeight);
+          handleClose();
+        })
       });
   }
 
@@ -49,10 +55,10 @@ const EditGoals = ({ handleClose }) => {
           <h2>Update your current weight:</h2>
         </label>
         <input
-          name="currentWeight"
+          name="weight"
           type="number"
-          value={editedWeight}
-          onChange={(e) => {setEditedWeight(e.target.value)}}
+          value={editedGoals.weight}
+          onChange={(e) => {setEditedGoals(editedGoals.weight = e.target.value)}}
         >
         </input>
         <h2>Set Your Daily Goals</h2>
@@ -62,8 +68,8 @@ const EditGoals = ({ handleClose }) => {
         <input
           name="waterGoal"
           type="number"
-          value={editedWaterGoal}
-          onChange={(e) => {setEditedWaterGoal(e.target.value)}}
+          value={editedGoals.water}
+          onChange={(e) => {setEditedGoals(editedGoals.water = e.target.value)}}
         >
         </input>
         <label>
@@ -72,8 +78,8 @@ const EditGoals = ({ handleClose }) => {
         <input
           name="caloriesGoal"
           type="number"
-          value={editedCaloriesGoal}
-          onChange={(e) => {setEditedCaloriesGoal(e.target.value)}}
+          value={editedGoals.calories}
+          onChange={(e) => {setEditedGoals(editedGoals.calories = e.target.value)}}
         >
         </input>
         <label>
@@ -82,28 +88,28 @@ const EditGoals = ({ handleClose }) => {
         <input
           name="proteinMacrosGoal"
           type="number"
-          value={editedProteinMacrosGoal}
-          onChange={(e) => {setEditedProteinMacrosGoal(e.target.value)}}
+          value={editedGoals.protein}
+          onChange={(e) => {setEditedGoals(editedGoals.protein = e.target.value)}}
         >
         </input>
         <label>
           <h3>Carbohydrate Macros</h3>
         </label>
         <input
-          name="carbsMacrosGoal"
+          name="carbsGoal"
           type="number"
-          value={editedCarbsMacrosGoal}
-          onChange={(e) => {setEditedCarbsMacrosGoal(e.target.value)}}
+          value={editedGoals.carbs}
+          onChange={(e) => {setEditedGoals(editedGoals.carbs = e.target.value)}}
         >
         </input>
         <label>
           <h3>Fats Macros</h3>
         </label>
         <input
-          name="fatsMacrosGoal"
+          name="fatsGoal"
           type="number"
-          value={editedFatsMacrosGoal}
-          onChange={(e) => {setEditedFatsMacrosGoal(e.target.value)}}
+          value={editedGoals.fats}
+          onChange={(e) => {setEditedGoals(editedGoals.fats = e.target.value)}}
         >
         </input>
         <label>
@@ -112,13 +118,13 @@ const EditGoals = ({ handleClose }) => {
         <input
           name="goalWeight"
           type="number"
-          value={editedGoalWeight}
-          onChange={(e) => {setEditedGoalWeight(e.target.value)}}
+          value={editedGoals.goalWeight}
+          onChange={(e) => {setEditedGoals(editedGoals.goalWeight = e.target.value)}}
         >
         </input>
         <button
           type="submit"
-          onClick={}
+          onClick={setUser}
         >Update Goals</button>
       </form>
     </div>
@@ -126,3 +132,132 @@ const EditGoals = ({ handleClose }) => {
 }
 
 export default EditGoals;
+
+// import React, { useContext, useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { AppContext } from '../../Context.jsx';
+
+
+// const EditGoals = ({ handleClose }) => {
+//   const {
+//     currentWeight, setCurrentWeight, waterGoal, setWaterGoal, caloriesGoal, setCaloriesGoal, proteinMacrosGoal, setProteinMacrosGoal, carbsMacrosGoal, setCarbsMacrosGoal, fatsMacrosGoal, setFatsMacrosGoal, goalWeight, setGoalWeight
+//   } = useContext(AppContext);
+
+//   const [editedWeight, setEditedWeight] = useState(currentWeight);
+//   const [editedWaterGoal, setEditedWaterGoal] = useState(waterGoal);
+//   const [editedCaloriesGoal, setEditedCaloriesGoal] = useState(caloriesGoal);
+//   const [editedProteinMacrosGoal, setEditedProteinMacrosGoal] = useState(proteinMacrosGoal);
+//   const [editedCarbsMacrosGoal, setEditedCarbsMacrosGoal] = useState(carbsMacrosGoal);
+//   const [editedFatsMacrosGoal, setEditedFatsMacrosGoal] = useState(fatsMacrosGoal);
+//   const[editedGoalWeight, setEditedGoalWeight] = useState(goalWeight);
+
+//   const postData = {
+//     user,
+//     currentWeight: editedWeight,
+//     waterGoal: editedWaterGoal,
+//     caloriesGoal: editedCaloriesGoal,
+//     proteinMacrosGoal: editedProteinMacrosGoal,
+//     carbsMacrosGoal: editedCarbsMacrosGoal,
+//     fatsMacrosGoal: editedFatsMacrosGoal,
+//     goalWeight: editedGoalWeight
+//   }
+
+//   const setGoals = (postData) => {
+//     axios.post('/goals', postData)
+//       .then(results => {
+//         setCurrentWeight(editedWeight);
+//         setWaterGoal(editedWaterGoal);
+//         setCaloriesGoal(editedCaloriesGoal);
+//         setProteinMacrosGoal(editedProteinMacrosGoal);
+//         setCarbsMacrosGoal(editedCarbsMacrosGoal);
+//         setFatsMacrosGoal(editedFatsMacrosGoal);
+//         setGoalWeight(editedGoalWeight);
+//         handleClose();
+//       });
+//   }
+
+//   return (
+//     <div>
+//       <h1>Edit Your Goals</h1>
+//       <form>
+//         <label>
+//           <h2>Update your current weight:</h2>
+//         </label>
+//         <input
+//           name="currentWeight"
+//           type="number"
+//           value={editedWeight}
+//           onChange={(e) => {setEditedWeight(e.target.value)}}
+//         >
+//         </input>
+//         <h2>Set Your Daily Goals</h2>
+//         <label>
+//           <h3>Water Consumption</h3>
+//         </label>
+//         <input
+//           name="waterGoal"
+//           type="number"
+//           value={editedWaterGoal}
+//           onChange={(e) => {setEditedWaterGoal(e.target.value)}}
+//         >
+//         </input>
+//         <label>
+//           <h3>Caloric Intake</h3>
+//         </label>
+//         <input
+//           name="caloriesGoal"
+//           type="number"
+//           value={editedCaloriesGoal}
+//           onChange={(e) => {setEditedCaloriesGoal(e.target.value)}}
+//         >
+//         </input>
+//         <label>
+//           <h3>Protein Macros</h3>
+//         </label>
+//         <input
+//           name="proteinMacrosGoal"
+//           type="number"
+//           value={editedProteinMacrosGoal}
+//           onChange={(e) => {setEditedProteinMacrosGoal(e.target.value)}}
+//         >
+//         </input>
+//         <label>
+//           <h3>Carbohydrate Macros</h3>
+//         </label>
+//         <input
+//           name="carbsMacrosGoal"
+//           type="number"
+//           value={editedCarbsMacrosGoal}
+//           onChange={(e) => {setEditedCarbsMacrosGoal(e.target.value)}}
+//         >
+//         </input>
+//         <label>
+//           <h3>Fats Macros</h3>
+//         </label>
+//         <input
+//           name="fatsMacrosGoal"
+//           type="number"
+//           value={editedFatsMacrosGoal}
+//           onChange={(e) => {setEditedFatsMacrosGoal(e.target.value)}}
+//         >
+//         </input>
+//         <label>
+//           <h3>Goal Weight</h3>
+//         </label>
+//         <input
+//           name="goalWeight"
+//           type="number"
+//           value={editedGoalWeight}
+//           onChange={(e) => {setEditedGoalWeight(e.target.value)}}
+//         >
+//         </input>
+//         <button
+//           type="submit"
+//           onClick={}
+//         >Update Goals</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default EditGoals;
