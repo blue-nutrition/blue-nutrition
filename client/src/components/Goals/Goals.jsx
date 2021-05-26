ca11ca7ffdd7388858f5034cd4a6445dae2ec1ed
a-clowes <clowes.alison@gmail.com>
2021-05-25 18:32:49 -0600

    Styling updated for main goals page.

--- client/src/components/Goals/Goals.jsx
+++ client/src/components/Goals/Goals.jsx
@@ -3,7 +3,20 @@ import { AppContext } from '../../Context.jsx';
 import { Modal } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import EditGoals from './EditGoals.jsx';
+
+
 
 const rand = () => (
   Math.round(Math.random() * 20) - 10
@@ -27,15 +40,38 @@ const useStyles = makeStyles(theme => ({
     overflow: 'scroll',
   },
   paper: {
-    position: 'absolute',
-    width: 450,
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: 'white',
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
+    width: '100%',
+    padding: '1.5rem'
   },
+  modalPaper: {
+    backgroundColor: 'white',
+    boxShadow: theme.shadows[5],
+    width: 'auto',
+    padding: '1.5rem'
+  }
 }));
 
+const goalsContainer = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
+
+const goalsStyle = {
+  width: '50%',
+  minHeight: '20%',
+  borderRadius: '5px',
+  marginBottom: '3vh',
+  marginTop: '4vh',
+  boxShadow: 'rgba(23, 51, 71, 0.3) 0px 19px 19px, rgba(23, 51, 71, 0.22) 0px 15px 12px',
+}
+
 let Goals = () => {
+
+
+
   const { userGoals } = useContext(AppContext);
   const [open, setOpen] = useState(false);
   const [modalStyle] = useState(getModalStyle);
@@ -51,39 +87,98 @@ let Goals = () => {
 
 
   return (
-    <div id="goals">
-      <h1>Your Nutrition Goals!</h1>
-      <Button
-        type="button"
-        onClick={handleOpen}
-        variant="contained"
-        color="primary"
-      >
-        <h6>Edit Goals</h6>
-      </Button>
-      <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-      >
-        <div
-            style={modalStyle}
-            className={classes.paper}
+    <div id="goals" className={"mainContainer"} style={goalsContainer}>
+      <div style={goalsStyle}>
+        <Typography variant="h3">Your Nutrition Goals</Typography>
+        <Button
+          type="button"
+          onClick={handleOpen}
+          variant="contained"
+          color="primary"
         >
-          <EditGoals
-            handleClose={handleClose}
-          />
-        </div>
-      </Modal>
-      <h2>Your current weight: {userGoals.weight} lbs (optional)</h2>
-      <h2>Your Goals</h2>
-      <h3>Water Consumption {userGoals.water} oz</h3>
-      <h3>Caloric Intake {userGoals.calories} kcal</h3>
-      <h3>Protein Macros {userGoals.protein} g</h3>
-      <h3>Carbohydrate Macros {userGoals.carbs} g</h3>
-      <h3>Fat Macros {userGoals.fats} g</h3>
-      <h3>Goal Weight {userGoals.goalWeight} lbs</h3>
+          <h6>Edit Goals</h6>
+        </Button>
+        <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description"
+        >
+          <div
+              style={modalStyle}
+              className={classes.paper}
+          >
+            <EditGoals
+              handleClose={handleClose}
+            />
+          </div>
+        </Modal>
+        <TableContainer component={Paper}>
+          <Table className={classes.paper}>
+            <TableHead>
+              <TableRow>
+                <TableCell><b>Your current weight:  (optional)</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableRow>
+              <TableCell>{userGoals.weight} lbs </TableCell>
+            </TableRow>
+          </Table>
+        </TableContainer>
+        <Typography variant="h6">Your Goals</Typography>
+        <TableContainer component={Paper}>
+          <Table className={classes.paper}>
+            <TableHead>
+              <TableRow>
+                <TableCell><b>Water Consumption</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableRow>
+              <TableCell>{userGoals.water} oz</TableCell>
+            </TableRow>
+            <TableHead>
+              <TableRow>
+                <TableCell><b>Caloric Intake</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableRow>
+              <TableCell>{userGoals.calories} kcal</TableCell>
+            </TableRow>
+            <TableHead>
+              <TableRow>
+                <TableCell><b>Protein Macros</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableRow>
+              <TableCell>{userGoals.protein} g</TableCell>
+            </TableRow>
+            <TableHead>
+              <TableRow>
+                <TableCell><b>Carbohydrate Macros</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableRow>
+              <TableCell>{userGoals.carbs} g</TableCell>
+            </TableRow>
+            <TableHead>
+              <TableRow>
+                <TableCell><b>Fat Macros</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableRow>
+              <TableCell>{userGoals.fats} g</TableCell>
+            </TableRow>
+            <TableHead>
+              <TableRow>
+                <TableCell><b>Goal Weight (optional)</b></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableRow>
+              <TableCell>{userGoals.goalWeight} lbs</TableCell>
+            </TableRow>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   )
 }
