421239cdbd52208a03e7c8a9e7eeef22b38224b3
Katie Brent <kab369@cornell.edu>
2021-05-28 09:43:23 -0600

    removing eslint errors

--- client/src/components/TrackProgress/Graphs/Graphs.jsx
+++ client/src/components/TrackProgress/Graphs/Graphs.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Calories from './CaloriesGraph.jsx';
 import Macros from './MacrosGraph.jsx';
